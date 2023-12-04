00eba502e17ad4cad21aa0744d6ade1e880d9b7d
Chris Fournier <cfournier@tigerconnect.com>
2023-12-04 11:10:49 -0500

    Nav and Layout updates
    Consolidated other application areas to "Tools" in the nav; made views visible in the bottom nav on mobile, but at the top of the inbox area on screens wider than 40rem (640px).

---
+++ assets/scripts/site.js
@@ -0,0 +1,26 @@
+// CONTENT LIST SAMPLE
+// $("li.content-sample").click(function () {
+//   $(".content--details").addClass("show");
+//   $("li.content-sample").addClass("selected");
+// });
+
+// $("span.back").click(function () {
+//   $(".content--details").removeClass("show");
+//   $("li.content-sample").removeClass("selected");
+// });
+
+
+// LOCATION SELECTION
+// window.addEventListener("DOMContentLoaded", function (theme) {
+//   localStorage.setItem('location', this.value);
+
+//   $('#location').change(function () {
+//     localStorage.setItem('location', this.value);
+//   });
+//   if (localStorage.getItem('location')) {
+//     $('#location').val(localStorage.getItem('location'));
+//   }
+
+//   var locationTitle = localStorage.getItem("location");
+//   document.getElementById("location-title").innerHTML = locationTitle;
+// });
