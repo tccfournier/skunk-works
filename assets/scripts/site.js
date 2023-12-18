78113ae9e957d09ce91f72e648573b7292cd3b16
Chris Fournier <cfournier@tigerconnect.com>
2023-12-18 10:43:52 -0500

    Migrated test work
    Including new layout and scss work

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,3 +1,26 @@
+// COLOR MODE CONTROLS
+window.addEventListener("DOMContentLoaded", function(theme) {
+  if(localStorage.getItem("theme") === "true") {
+    $("html").toggleClass("dark");
+    $("input.switch").prop("checked",true);
+  }
+  else {
+    localStorage.setItem("theme",false);
+  }
+});
+$("input.switch").click(function() {
+  $("html").toggleClass("dark");
+  if(localStorage.getItem("theme") === "true") {
+    $("input.switch").prop("checked",false);
+    localStorage.setItem("theme",false);
+  }
+  else {
+    $("input.switch").prop("checked",true);
+    localStorage.setItem("theme",true);
+  }
+});
+
+
 // CONTENT LIST SAMPLE
 // $("li.content-sample").click(function () {
 //   $(".content--details").addClass("show");
