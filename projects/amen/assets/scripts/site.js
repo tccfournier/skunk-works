ad8608bd38925ccfe90f122c54832d7e2b032183
Chris Fournier <cfournier@tigerconnect.com>
2024-06-11 13:28:49 -0400

    AMEN 021 template ready

--- projects/amen/assets/scripts/site.js
+++ projects/amen/assets/scripts/site.js
@@ -19,3 +19,8 @@ $('input#switch-theme').click(function () {
     localStorage.setItem('theme', true);
   }
 });
+
+// TABS
+$("li.tab-item button#tab-label-source").click(function() { $("li.tab-item button.is-active").removeClass("is-active").attr("aria-selected", false); $("li.tab-item button#tab-label-source").addClass("is-active").attr("aria-selected", true); $(".panels .panel.is-current").removeClass("is-current").attr("aria-hidden", true); $(".panels .panel#tab-panel-source").addClass("is-current").attr("aria-hidden", false); });
+$("li.tab-item button#tab-label-mapping").click(function() { $("li.tab-item button.is-active").removeClass("is-active").attr("aria-selected", false); $("li.tab-item button#tab-label-mapping").addClass("is-active").attr("aria-selected", true); $(".panels .panel.is-current").removeClass("is-current").attr("aria-hidden", true); $(".panels .panel#tab-panel-mapping").addClass("is-current").attr("aria-hidden", false); });
+$("li.tab-item button#tab-label-data").click(function() { $("li.tab-item button.is-active").removeClass("is-active").attr("aria-selected", false); $("li.tab-item button#tab-label-data").addClass("is-active").attr("aria-selected", true); $(".panels .panel.is-current").removeClass("is-current").attr("aria-hidden", true); $(".panels .panel#tab-panel-data").addClass("is-current").attr("aria-hidden", false); });
